4641b973a6d2e4dd29c87d162bb7d4c4b15a7653
Maggie Chen <97855985+maggieyc@users.noreply.github.com>
2023-11-08 14:44:47 -0500

    testing react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import "./App.css"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+      
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Landing() {
+  return (
+    <div>
+      <h1>Maggie Chen</h1>
+      <Link to="/about">Arrow</Link>
+    </div>);
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/projects">Arrow</Link>
     </div>
   );
 }
 
-export default App;
+function Projects() {
+  return <h2>Users</h2>;
+}
