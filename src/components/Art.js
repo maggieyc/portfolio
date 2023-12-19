88e1d624461d6433416b9d55a0449cf4f54abd08
Maggie Chen <97855985+maggieyc@users.noreply.github.com>
2023-12-19 13:25:06 -0500

    fix gallery page styles

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,17 +1,20 @@
 import AnimatedPage from "./AnimatedPage";
 import {Link} from "react-router-dom";
 export default function Art(){
+  
   return(
     <div className="container">
       <AnimatedPage>
         <div className="gallery-container">
           <h2>Gallery</h2>
         
-          <AnimatedPage className="gallery">
-            <img src="images\man-in-chair.jpg" className="gallery-img" alt="painting of a man in chair"></img>
-            <img src="images\saxophone.jpg" className="gallery-img" alt="painting of a man playing saxophone"></img>
-            <img src="images\underwater.jpg" className="gallery-img" alt="painting of an underwater city"></img>
-            <img src="images\beaver-tails.jpg" className="gallery-img" alt="painting of byward market in Ottawa"></img>
+          <AnimatedPage>
+            <div className="gallery">
+              <img src="images\man-in-chair.jpg" className="gallery-img" alt="painting of a man in chair"></img>
+              <img src="images\saxophone.jpg" className="gallery-img" alt="painting of a man playing saxophone"></img>
+              <img src="images\underwater.jpg" className="gallery-img" alt="painting of an underwater city"></img>
+              <img src="images\beaver-tails.jpg" className="gallery-img" alt="painting of byward market in Ottawa"></img>
+            </div>
           </AnimatedPage>
         </div>
       </AnimatedPage>
