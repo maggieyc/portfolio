e4252d64003d5b34f87397d1bd9f0e2c02528673
Maggie Chen <97855985+maggieyc@users.noreply.github.com>
2023-12-19 14:02:51 -0500

    fix responsive gallery page

--- src/components/EtchSketch.js
+++ src/components/EtchSketch.js
@@ -5,8 +5,9 @@ export default function EtchSketch(){
     <div className="page-container">
       <Link to="/projects" className="interactive link">Back</Link>
       <h2 className="page-title">Etch-A-Sketch</h2 >
-      <p>The Odin Project</p>
-      <p>Fun Fact: I created the background on Inkscape!</p>
+      <p>By: Maggie Chen</p>
+      <p>Created during The Odin Project.</p>
+      <p>Fun Fact: I also created the background on Inkscape!</p>
       <p>(Click below and play with arrow keys)</p>
       <iframe title="Etch-A-Sketch" src="https://maggieyc.github.io/odin-etch-a-sketch/"></iframe>
       <br />
