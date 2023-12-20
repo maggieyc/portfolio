088fde5f5583889d72bb6993a9f8ff5c228f7f87
Maggie Chen <97855985+maggieyc@users.noreply.github.com>
2023-12-20 08:19:36 -0800

    add linkedin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,12 +11,18 @@ export default function Contact(){
             <ul className="list">
               <li >
                 <h3>
-                  <a className="interactive link" href="https://github.com/maggieyc">GitHub: @maggieyc</a>
+                  <a target="_blank"className="interactive link" href="https://github.com/maggieyc">GitHub: @maggieyc</a>
                 </h3>
               </li>
 
               <li>
                 <h3>maggieyc2@gmail.com</h3>
+              </li>
+
+              <li>
+                <h3>
+                  <a target="_blank" className="interactive link" href="https://www.linkedin.com/in/maggie-chen-8aa57322b/">Linkedin</a>
+                </h3>
               </li>
             </ul>
           </AnimatedPage>
